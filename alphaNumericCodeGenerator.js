96a349fbe0b923a7e1dfb8fd9029989ffe0231fa
Corey Trice <corey@coreytrice.com>
2014-01-22 10:50:14 -0600

    Tabs to spaces

--- alphaNumericCodeGenerator.js
+++ alphaNumericCodeGenerator.js
@@ -1,8 +1,8 @@
-var	  alphNum	= 'abcdefghijklmnopqrstuvwxyz0123456789'
-	, result	= ''
-	, length	= 16
+var   alphNum    = 'abcdefghijklmnopqrstuvwxyz0123456789'
+    , result     = ''
+    , length     = 16
 ;
 
 for(var i = length; i > 0; --i){
-	result += alphNum[Math.round(Math.random() * (alphNum.length - 1))];
+    result += alphNum[Math.round(Math.random() * (alphNum.length - 1))];
 }
